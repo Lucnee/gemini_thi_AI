fb59fa0a03e2f1eb9316754b3f6466c1d81fa87b
ThuongNVa <toanrui64@gmail.com>
2024-06-20 18:22:27 +0700

    Em Thuong Test Git #1

---
+++ test.py
@@ -0,0 +1,10 @@
+print('Tài thích Thương !!!')
+
+def test_func(loi_chao):    
+    if loi_chao.length > 10:
+        print('Tài yêu Thương vãi cục cưng nhà nó !!!')
+    else:
+        print('Tài đéo yêu Thương !!!')
+    return print('Chúc Tài may mắn !!!')
+
+test_func('Toi ten Bui Duc Tai !!!')
